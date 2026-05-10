b485ac2a48ffa43c88106551b923a8d9f1f410ab
xNoctis25 <Jeanpb25@gmail.com>
2026-05-09 20:45:41 -0500

    Cleanup: Remove test scripts

--- backend/Core/test_insert.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({path: '.env'});
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL, ssl: true });
-pool.query("INSERT INTO notifications (event_type, message) VALUES ('market_alert', 'TEST')").then(res => { console.log('Insert success'); pool.end(); }).catch(console.error);
